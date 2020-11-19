59d082dfc53fd4d3df4aee893153ea12c5581afe
greyhexy <whatoget@gmail.com>
2020-11-19 14:00:38 +0800

    Solved 97-implement-strStr problem

---
+++ array/two-pointer/97-implement-strStr.cc
@@ -0,0 +1,41 @@
+/**
+ * Implement strstr.
+ * const char* strstr(const char* haystack, const char* needle)
+ * Return the index of the first occurrence of needle in haystack,
+ * or -1 if needle is not part of haystack.
+ * Note: If needle is an empty string, return 0.
+ */
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+int strStr(string haystack, string needle)
+{
+    if (needle.empty()) return 0;
+    int index, l1, l2, i, j;
+    l1 = haystack.size();
+    l2 = needle.size();
+    index = -1;
+    for(i = 0; i + l2 <= l1; ++i) {
+        for (j = i; j < i + l2 && haystack[j] == needle[j-i]; ++j)
+            ;
+        if (j == i + l2) {
+            index = j - l2;
+            break;
+        }
+    }
+    return index;
+}
+
+int main()
+{
+    assert(strStr("aaa", "a") == 0);
+    assert(strStr("aaa", "") == 0);
+    assert(strStr("abcde", "fgh") == -1);
+    assert(strStr("abcbc", "bc") == 1);
+
+    return 0;
+}
